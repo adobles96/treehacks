4b0855628fe6e057fa51f637a2a73895a1c7ce00
Connor <normand.connor@gmail.com>
2018-02-17 14:23:14 -0800

    Integrates React front with current Flask backend

--- emissions/emissions.py
+++ emissions/emissions.py
@@ -4,7 +4,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 	render_template, flash
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
 app.config.from_object(__name__)
 
 
@@ -46,13 +46,9 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-
-
-
-
 @app.route('/')
 def index():
-    return 'Hello!'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
